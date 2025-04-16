aefa794e1e284b836c1bed6891e13823e618c7db
varchunn <varvaram186@gmail.com>
2025-04-16 21:47:35 +0300

    Initial Commit

---
+++ http/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  http
+//
+//  Created by Варя Мінєнкова on 16.04.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
